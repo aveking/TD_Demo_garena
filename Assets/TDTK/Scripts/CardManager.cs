1f1fc73ec00b0ebcd914fde87f42fe2dabb7ec4f
Jennifer Li <88904004@qq.com>
2023-09-12 20:57:12 +0800

    Add draw weights

--- Assets/TDTK/Scripts/CardManager.cs
+++ Assets/TDTK/Scripts/CardManager.cs
@@ -20,6 +20,8 @@ namespace TDTK
     {
         public int drawNum = 10;
 
+        private int drawTimes = 0;
+
         private Dictionary<CardType, Card> myCards = new Dictionary<CardType, Card>();
 
         // Start is called before the first frame update
@@ -56,14 +58,104 @@ namespace TDTK
             get { return myCards.Count; }
         }
 
+
+        private int GenQuality()
+        {
+            int quality = 0;
+            int num = Random.Range(0, 10);
+
+            if (drawTimes < 10)
+            {
+                quality = 0;
+            }
+            else if (drawTimes<20)
+            {
+                if (num < 7)     
+                    quality = 0;
+                else
+                    quality = 1;
+            }
+            else if (drawTimes<50)
+            {
+                if (num < 2)
+                    quality = 0;
+                else if (num<6)
+                    quality = 1;
+                else
+                    quality = 2;
+            }
+            else if (drawTimes < 100)
+            {
+                if (num < 2)
+                    quality = 0;
+                else if (num < 5)
+                    quality = 1;
+                else if (num< 9 )
+                    quality = 2;
+                else
+                    quality = 3;
+            }
+            else if (drawTimes < 150)
+            {
+                if (num < 1)
+                    quality = 0;
+                else if (num < 3)
+                    quality = 1;
+                else if (num < 7)
+                    quality = 2;
+                else if (num < 9)
+                    quality = 3;
+                else
+                    quality = 4;
+            }
+            else
+            {
+                if (num < 1)
+                    quality = 0;
+                else if (num < 2)
+                    quality = 1;
+                else if (num < 4)
+                    quality = 2;
+                else if (num < 7)
+                    quality = 3;
+                else if (num < 9)
+                    quality = 4;
+                else
+                    quality = 5;
+            }
+
+            return quality;
+        }
+
+        int GenLevel()
+        {
+            int level = 0;
+            int num = Random.Range(0, 20);
+
+            if (num < 8)
+                level = 0;
+            else if (num < 14)
+                level = 1;
+            else if (num < 17)
+                level = 2;
+            else if (num < 19)
+                level = 3;
+            else
+                level = 4;
+
+            return level;
+        }
+
         public Card DrawCard()
         {
             Card card = new Card
             {
                 cardType = (CardType)Random.Range(0, 8),
-                Quality = Random.Range(0, 6),
-                Level = Random.Range(0, 6)
+                Level = GenLevel(),
+                Quality = GenQuality()
             };
+
+            drawTimes++;
 
             return card;
         }
